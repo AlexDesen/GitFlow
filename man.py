078a44b31cf2d058c443b8450412f54e323c4ffa
ALEX NEVES <alexprogran@gmail.com>
2023-02-11 02:27:33 -0300

    Adicionando persistência de dados no projeto

--- man.py
+++ man.py
@@ -1,23 +1,32 @@
- 
-
+from persitencia import ler_dados_salvos, salvar_dado_disco
 itens_estoque = []
 
 def adicionar_itens_estoque():   
     nome = input('Nome do iten: ')
     descricao = input('Descrição do iten: ')
     quantidade = int(input('Quantidade do iten: '))
-    itens = {'nome':nome, 'descricao':descricao,'quantidade':quantidade}
+    itens = {
+        'nome':nome, 
+        'descricao':descricao,
+        'quantidade':quantidade
+        }
     itens_estoque.append(itens)
 
 
 def listar_itens_estoque():
     print('------------------------------------')
     for item in itens_estoque:
-        print(f"Nome:{item['nome']}")
-        print(f"Quantindade:{item['quantidade']}")
+
+        print(f'Nome:{item["nome"]}')
+        print(f'Quantindade:{item["quantidade"]}')
+       
     print('------------------------------------')
 
+
+
+
 if __name__ =="__main__":
+    itens_estoque = ler_dados_salvos()
     print('Sistema de estoque Ultima School\n')
     opcao = 0
     while opcao != 3:
@@ -33,7 +42,9 @@ if __name__ =="__main__":
         elif opcao == 2:
            listar_itens_estoque()
         elif opcao == 3:
+            salvar_dado_disco(itens_estoque)  
             print('Obrigado por usar nosso sistema') 
+          
         else:      
             print('Opção inválida.Tente novamente')
            
